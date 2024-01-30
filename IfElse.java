4192b5d6ed232a1249e58bd1baeea145e6bc1e9d
HelgaFaisa <158024195+HelgaFaisa@users.noreply.github.com>
2024-01-30 09:44:40 +0700

    ifelse

---
+++ IfElse.java
@@ -0,0 +1,19 @@
+
+package ifelse;
+
+
+public class IfElse {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String language = "Java";
+        
+        if (language == "Java") {
+            System.out.println("Best Programing Language");
+            
+        }
+        
+    }
+    
+}
